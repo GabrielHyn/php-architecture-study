f64898e1960068fd65bb6cb51742f02a4a4ccf54
GabrielHyn <ggv1@aluno.ifnmg.edu.br>
2026-01-04 20:06:11 -0300

    refactor: extract HMTL from ProductController to listing view

--- project-stock/app/Http/Controllers/ProductController.php
+++ project-stock/app/Http/Controllers/ProductController.php
@@ -8,14 +8,7 @@ class ProductController extends Controller
 {
     public function list()
     {
-        $html = '<h1>Product Listing with Laravel</h1>';
-        $html .= '<ul>';
         $products = DB::select('select * from products');
-        foreach ($products as $p) {
-            $html .= '<li> Name: ' . $p->name . ',
-                        Description: ' . $p->description . '</li>';
-        }
-        $html .= '</ul>';
-        return $html;
+        return view('listing', ['products' => $products]);
     }
 }
