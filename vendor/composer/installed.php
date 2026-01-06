a4f99b00199057b701a57b7dca44f2872cc7da5e
GabrielHyn <ggv1@aluno.ifnmg.edu.br>
2026-01-06 20:38:01 -0300

    temporary backup

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gabriel/project-payment',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0e484208350832a5d19d3d112ebca05ca54ce52d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gabriel/project-payment' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0e484208350832a5d19d3d112ebca05ca54ce52d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'laravel/pint' => array(
+            'pretty_version' => 'v1.27.0',
+            'version' => '1.27.0.0',
+            'reference' => 'c67b4195b75491e4dfc6b00b1c78b68d86f54c90',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../laravel/pint',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
